9e3af8a15f55606fda769f29c7923fbd420ff5fb
pamsfields <jh5811zj@go.minneapolis.edu>
2018-01-09 16:21:01 -0600

    Initial commit, still need to update to get number guess to stop loop

---
+++ Lab1GuessTheNumberPamFields1-9-2018.py
@@ -0,0 +1,21 @@
+#Pam Fields ITEC 2905-01 Lab 1 Hello Python
+#1/9/2018
+# Problem 1 Guess the number
+print("Guessing Game")
+
+import random
+# pulling random into code
+
+x = random.randint(1,100)
+#setting up random number
+guess = int(input("Pick a number between 1 and 100 "))
+#user input for game
+while guess != x:
+  if guess > x:
+    print("Your guess is too low.")
+    input("Pick a number between 1 and 100 ")
+  else guess < x:
+    print("Your guess is too high")
+    input("Pick a number between 1 and 100 ")
+
+print("You got it right!")
